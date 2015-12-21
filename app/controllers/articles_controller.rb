3198b80964741da379dad36455f7145482029afe
Kitwana <kitwana@akildev.com>
2015-12-21 01:44:11 +0000

    Updated articles_controller's befor_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  before_action :set_article, only: [:edit, :update, :show, :destory]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-   
+
   end
 
   def create
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    
+
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -38,7 +38,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
